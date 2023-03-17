44172eb6f330daa41a196e8254b69cef131b7379
Will Powell <me@willpowell.uk>
2023-03-17 17:15:08 +0000

    Now stores dataframes for speed of use.

--- Utilities.py
+++ Utilities.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import os
+import pickle
+
 def progress_bar(current, total, bar_length=20):
     fraction = current / total
 
@@ -7,3 +11,28 @@ def progress_bar(current, total, bar_length=20):
     ending = '\n' if current == total else '\r'
 
     print(f'Progress: [{arrow}{padding}] {int(fraction*100)}%', end=ending)
+
+def save_dataframe(df, file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "wb") as f:
+        pickle.dump(df, f)
+    print(f"Dataframe saved to {file_path}")
+
+def load_dataframe(file_path):
+    try:
+        with open(file_path, 'rb') as f:
+            df = pickle.load(f)
+    except:
+        return False
+    print(f"Using stored dataframe from {file_path}")
+    return df
+    
+
+if __name__=='__main__':
+    df1 = pd.DataFrame()
+    df2 = pd.DataFrame()
+    dfs = {}
+    dfs[0] = df1
+    dfs[1] = df2
+    save_dataframe(dfs, 'Data/test.pkl')
+    df = load_dataframe('Data/test.pkl')
